14a1388c1122b9c334a19e439f02c8da33098fc6
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-13 21:04:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,7 +6,7 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end 
-songs = ["Phoenix - 1901", ]
+songs = ["Phoenix - 1901", "Tokyo Police Club - Wait Up"]
 def list(songs) 
   songs.each_with_index |item, index| do 
    puts "#{index + 1}. #{item}"
