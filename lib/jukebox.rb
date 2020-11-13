f0036ca824c82a4be9c9d2ceb0f4262328538125
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-13 21:15:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,8 +32,6 @@ def play(songs)
   songs.each_with_index do |song, index| 
     if user_input === song || user_input === index + 1
       puts "Playing #{song}"
-    else 
-      puts "Invalid input, please try again"
     end 
   end 
 end 
