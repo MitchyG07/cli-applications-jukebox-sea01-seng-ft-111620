2ecc36268a52c8ae4abea1ecc26f90183b3ccf86
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-13 22:10:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,5 +42,26 @@ def exit_jukebox
   puts "Goodbye"
 end 
 
+def run 
+  puts "Please enter a command:"
+  
+  until user_inpt === "exit" do 
+    
+    user_input = gets.strip 
+  
+    case user_input 
+      when "help"
+        help 
+      when "list"
+        list(songs)
+      when "play"
+       play(songs)
+      when "exit"
+        exit_jukebox
+  end 
+  
+  
+  
+
 
   
